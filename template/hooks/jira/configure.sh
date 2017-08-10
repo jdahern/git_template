5572aff93b5f16a618b339032f4299c3e7234b19
Jeff <jeffery.ahern@gmail.com>
2017-08-10 15:23:07 -0700

    Update configure.sh

--- template/hooks/jira/configure.sh
+++ template/hooks/jira/configure.sh
@@ -4,7 +4,7 @@
 main() {
     if hook_is_enabled jira
     then
-	read -p "Regex to use for checking the message with ( \"^(DEV-[0-9]+|merge)\" )"
+	read -p "Regex to use for checking the message with. Default: \"^(DEV-[0-9]+|merge)\"? "
 	if [ -z $REPLY ]; then
 	    REPLY="^(DEV-[0-9]+|merge)"
 	fi
